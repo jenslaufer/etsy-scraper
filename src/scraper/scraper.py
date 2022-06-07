79dc90bd2d4780bfd504ea2acfc3806112bdcbcc
Jens Laufer <jenslaufer@gmail.com>
2022-06-07 08:45:09 +0200

    implementation + test

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -3,31 +3,47 @@ from datetime import date
 from datetime import datetime
 import time
 import random
-from providers import Fetch
+from scraper.providers import Fetch
 from scraper.parser import SearchParser
-from storage import Storage
+from scraper.storage import Storage
 from math import ceil
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 class Scraper:
 
-    def __init__(self, fetcher: Fetch, storage: Storage, search_parser: SearchParser):
+    def __init__(self, fetcher: Fetch, storage: Storage, search_parser: SearchParser, num_workers=10):
         super().__init__()
         self.storage = storage
         self.search_parser = search_parser
         self.fetcher = fetcher
         self.search_url_templ = "https://www.etsy.com/de/search?q={}&page={}&ref=pagination"
         self.results_per_page = 64
+        self.num_workers = num_workers
 
-    def scrape(self, keyword, fetch=True):
+    def _fetch(self, url):
+        content = self.fetcher.fetch(url)
+        self.storage.save_file(url, content)
 
-        num_pages = 1
+        return content
 
-        for page in range(0, num_pages+1):
-            search_url = self.search_url_templ.format(keyword, page)
-            content = self.fetcher.fetch(search_url)
-            self.storage.save_file(search_url, content)
-            result = self.search_parser.parse(content)
-            num_pages = ceil(result["num_results"]/64)
+    def scrape(self, keyword, num_pages=None, fetch=True):
+        futures = []
+        pages = []
 
-        return {}
+        with ThreadPoolExecutor(max_workers=self.num_workers) as executor:
+            if num_pages is None:
+                num_pages = 1
+
+            for page in range(0, num_pages):
+                search_url = self.search_url_templ.format(keyword, page)
+                pages.append(search_url)
+                futures.append(executor.submit(self._fetch, url=search_url))
+
+                if page == 0 and as_completed(futures):
+                    content = self._fetch(search_url)
+                    result = self.search_parser.parse(content)
+                    if num_pages is None:
+                        num_pages = ceil(result["num_results"]/64)
+
+        return pages
---
+++ test/scraper_test.py
@@ -0,0 +1,27 @@
+import context
+from unittest import TestCase
+from scraper.storage import MongoStorage
+from scraper.parser import SearchParser
+from scraper.providers import RequestsFetch, Fetch
+from scraper.scraper import Scraper
+from pymongo_inmemory import MongoClient
+
+
+class ScraperTest(TestCase):
+    def setUp(self):
+        db = MongoClient()["test"]
+        self.storage = MongoStorage(db)
+        self.scraper = Scraper(
+            RequestsFetch(),
+            self.storage,
+            SearchParser())
+
+    def test_scraping(self):
+        num_pages = 2
+        pages = self.scraper.scrape("programming shirts", num_pages=num_pages)
+        self.assertEquals(num_pages, len(pages))
+        for page in pages:
+            self.assertTrue(self.storage.exists(page))
+            print(self.storage.get_file_content(page))
+            self.assertTrue(
+                "html" in self.storage.get_file_content(page).decode("utf-8"))
