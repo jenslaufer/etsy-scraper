19ff5177d54df588bba862e3f89f3b9dee285834
Jens Laufer <jenslaufer@gmail.com>
2022-06-25 06:34:20 +0200

    fixed

--- src/app.py
+++ src/app.py
@@ -4,8 +4,8 @@ from scraper.providers import RequestsFetch
 from scraper.storage import MongoStorage
 from scraper.parser import DetailsParser, SearchParser
 import logging
-
 from eve import Eve
+from concurrent.futures import ThreadPoolExecutor
 
 
 debug = bool(os.environ.get('DEBUG', "True"))
@@ -22,6 +22,20 @@ app = Eve()
 with app.app_context():
     scraper = Scraper(RequestsFetch(), MongoStorage(app.data.driver.db),
                       SearchParser(), DetailsParser())
+
+
+def _scrape(query, num_pages=None):
+    scraper.scrape(query, num_pages)
+
+
+def insert_scrapes(scrapes):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for scrape in scrapes:
+            executor.submit(_scrape, query=scrape["query"], num_pages=2)
+
+
+app.on_insert_scrapes += insert_scrapes
+
 
 if __name__ == "__main__":
     app.run(host=host, debug=debug)
